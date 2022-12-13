6711e227b2d4ea19a979ed5348cb236e956ba7b7
komtriangle <komlevdanila742@gmail.com>
2022-12-13 19:49:12 +0300

    dc

--- Back/XmasHack.API/CRUD_API/Models/Requests/SaveDocsRequest.cs
+++ Back/XmasHack.API/CRUD_API/Models/Requests/SaveDocsRequest.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace XmasHack.API.CRUD_API.Models.Requests
+﻿namespace XmasHack.API.CRUD_API.Models.Requests
 {
     public class SaveDocsRequest
     {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
     }
 }
