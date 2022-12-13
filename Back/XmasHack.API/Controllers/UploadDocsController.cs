7df686bf1532c4b71cde6b74b0e4129499b09a80
komtriangle <komlevdanila742@gmail.com>
2022-12-13 21:03:29 +0300

    back:

--- Back/XmasHack.API/Controllers/UploadDocsController.cs
+++ Back/XmasHack.API/Controllers/UploadDocsController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Options;
 using XmasHack.API.Configuration;
 using XmasHack.API.CRUD_API;
 using XmasHack.API.CRUD_API.Models.Requests;
+using XmasHack.API.Models;
 
 namespace XmasHack.API.Controllers
 {
@@ -43,6 +44,32 @@ namespace XmasHack.API.Controllers
             }
             return Ok("Документы успешно сохранены");
 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllFiles()
+        {
+            return Ok(new FileResponse[]
+            {
+                new FileResponse()
+                {
+                    FileName="TD-2435_ux.pdf",
+                    FilePath="5cf413ba-2952-4d23-9378-4366ab6ea365-TD-2435_ux.pdf",
+                    Type = "Type 1"
+                },
+                new FileResponse()
+                {
+                    FileName ="Семинар(занания).docx",
+                    FilePath="9c4daeee-aa9e-4ed5-92d4-185060ad5618-Семинар(занания).docx",
+                    Type = "Type 2"
+                },
+                new FileResponse()
+                {
+                    FileName ="vertopal.com_Untitled52.pdf",
+                    FilePath="a6f7415c-bdb1-498f-b9fc-250dd32f71bf-vertopal.com_Untitled52.pdf",
+                    Type ="Type 3"
+                }
+            });
         }
 
         private  async Task SaveDocsToFolder(IFormFile file, string docsName)
