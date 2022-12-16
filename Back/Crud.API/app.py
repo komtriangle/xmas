d3ebfd358dfb58e02ea8a41bb4fb4f3a2aab905d
komtriangle <komlevdanila742@gmail.com>
2022-12-16 17:50:55 +0300

    a

--- Back/Crud.API/app.py
+++ Back/Crud.API/app.py
@@ -179,4 +179,4 @@ def get_all_docs(id):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
